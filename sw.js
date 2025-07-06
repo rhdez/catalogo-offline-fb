7c0c1ffff55306c895212bef72d52a6026211d15
Ricardo Hernández <rhdez.g@gmail.com>
2025-07-05 21:44:11 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('v1').then(c=>c.addAll([ './','index.html','productos.json','https://cdn.jsdelivr.net/npm/@ericblade/quagga2@1.2.6/dist/quagga.min.js' ])));
+  e.waitUntil(caches.open('v1').then(c=>c.addAll([
+    './',
+    'index.html',
+    'productos.json',
+    'https://cdn.jsdelivr.net/npm/@ericblade/quagga2@1.2.6/dist/quagga.min.js'
+  ])));
 });
 self.addEventListener('fetch',e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
